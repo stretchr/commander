552f79bbe197d4fc5e2e9d5a8cfa1559ac4807e6
Mat Ryer <matryer@mac.com>
2012-11-22 12:17:54 +0000

    Added benchmark test to prove predefining arrays is WAY quicker than using `append`

---
+++ building_array_benchmark_test.go
@@ -0,0 +1,33 @@
+package commander
+
+import (
+	"testing"
+)
+
+const SizeOfArray int = 1000000
+
+func BenchmarkBuildArrayWithAppend(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+
+		var bigArray []int
+		for count := 0; count < SizeOfArray; count++ {
+			bigArray = append(bigArray, count)
+		}
+
+	}
+
+}
+
+func BenchmarkBuildArrayWithPredefinedSize(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+
+		var bigArray []int = make([]int, SizeOfArray)
+		for count := 0; count < SizeOfArray; count++ {
+			bigArray[count] = count
+		}
+
+	}
+
+}
