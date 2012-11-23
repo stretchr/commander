b123d5ba9ad10b5bfe5382733636d690224dc295
Tyler Bunnell <tylerbunnell@gmail.com>
2012-11-23 10:38:10 -0700

    Map a command

--- commander.go
+++ commander.go
@@ -1,1 +1,30 @@
 package commander
+
+import (
+	"sync"
+)
+
+// Commander provides methods and functionality to create a command line
+// interface quickly and easily.
+type Commander struct {
+	// commands contains all the mapped commands
+	commands []*command
+}
+
+// initOnce is used to guarantee that the sharedCommander is initialized only once.
+var initOnce sync.Once
+
+// sharedCommander is the shared instance of the Commander type
+var sharedCommander *Commander
+
+func Map(definition string, handler Handler) {
+
+	initOnce.Do(func() {
+		sharedCommander = new(Commander)
+	})
+
+	newCommand := makeCommand(definition, handler)
+
+	sharedCommander.commands = append(sharedCommander.commands, newCommand)
+
+}
--- commander_test.go
+++ commander_test.go
@@ -1,1 +1,15 @@
 package commander
+
+import (
+	"github.com/stretchrcom/testify/assert"
+	"testing"
+)
+
+func TestCommander_Map(t *testing.T) {
+
+	Map(commandString, func(map[string]interface{}) {
+	})
+
+	assert.Equal(t, len(sharedCommander.commands), 1)
+
+}
