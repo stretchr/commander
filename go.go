ab107661a390b2786d9d64715fa919281d364e82
Mat Ryer <matryer@mac>
2013-01-15 12:26:45 -0700

    added comments for new Go wrapper command

--- go.go
+++ go.go
@@ -1,5 +1,15 @@
 package commander
 
+// Go wraps calls to `commander.Map` (which should be placed in the func argument) and
+// initializes Commander and executes the commands.
+//
+// Usage
+//
+//     commander.Go(func(){
+//     
+//	     // make calls to commander.Map here
+//     
+//     })
 func Go(mappings func()) {
 
 	// ensure commander is initialized
